b1e2c41ef2c0109752f28678ec96d538e2f70144
Lohanny Carvalho <lohanny.silva2@etec.sp.gov.br>
2024-04-23 17:29:48 -0300

    criei o contatos

---
+++ app/Http/Controllers/ContatosController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contato;
+use Exception;
+use Illuminate\Http\Request;
+
+class ContatosController extends Controller
+{
+    /**
+     * Renderiza a página de contato
+     * 
+     * @return View
+    */
+    public function contato()
+    {
+        return view('contato');
+    }
+
+    /**
+     * 
+    */
+    public function enviarFormulario(Request $request)
+    {
+       try {
+            Contato::create([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'telefone' => $request->telefone,
+            'assunto' => $request->assunto,
+            'mensagem' => $request->mensagem
+            ]);
+
+            return back()->withSuccess('Formulário enviado');
+        } catch (Exception $exception) {
+            return back()->withErrors($exception->getMessage());
+       }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContatosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contato',[ContatosController::class,'contato']);
+Route::post('contato',[ContatosController::class,'enviarFormulario']);
